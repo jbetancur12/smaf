81e5fdeac150eb262b4d102cd0b85a2b249dffd3
Jorge Betancur <jorge.betancur@teads.tv>
2024-01-17 12:41:36 -0500

    dates

--- src/pages/TemplatesPage/components/DatePickerRange.tsx
+++ src/pages/TemplatesPage/components/DatePickerRange.tsx
@@ -1,9 +1,15 @@
-import { Button } from '@mui/material';
-import React, { forwardRef, useState } from 'react';
+import { Button } from "@mui/material";
+import dayjs from "dayjs";
+import React, { forwardRef, useState } from "react";
 import DatePicker, { CalendarContainer } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-function manipulateDate(oldDate: Date, offset: number, offsetType: string, operation: string): Date {
+function manipulateDate(
+  oldDate: Date,
+  offset: number,
+  offsetType: string,
+  operation: string
+): Date {
   const newDate = new Date(oldDate.getTime());
 
   const operators: Record<string, (val: number, off: number) => number> = {
@@ -18,14 +24,16 @@ function manipulateDate(oldDate: Date, offset: number, offsetType: string, opera
     H: "Hours",
   };
 
-    if (operation === "add" || operation === "rest") {
-      (newDate as any)[`set${offsetTypes[offsetType]}`](
-        operators[operation]((oldDate as any)[`get${offsetTypes[offsetType]}`](), offset)
-      );
-    } else {
-       return oldDate;
-    }
-
+  if (operation === "add" || operation === "rest") {
+    (newDate as any)[`set${offsetTypes[offsetType]}`](
+      operators[operation](
+        (oldDate as any)[`get${offsetTypes[offsetType]}`](),
+        offset
+      )
+    );
+  } else {
+    return oldDate;
+  }
 
   return newDate;
 }
@@ -42,14 +50,13 @@ function manipulateDate(oldDate: Date, offset: number, offsetType: string, opera
 //   return oldDate;
 // }
 
-
-
-
 const DatePickerRange: React.FC<{
-  datesQuery: (start: Date, end: Date) => void
-  custom: Boolean
-}> = ({  datesQuery, custom }) => {
-  const [startDate, setStartDate] = useState<Date>(manipulateDate(new Date(), 7, "D", "rest"));
+  datesQuery: (start: Date, end: Date) => void;
+  custom: Boolean;
+}> = ({ datesQuery, custom }) => {
+  const [startDate, setStartDate] = useState<Date>(
+    manipulateDate(new Date(), 7, "D", "rest")
+  );
   const [endDate, setEndDate] = useState<Date>(new Date());
 
   // Assuming you have a `useGetDashboardGraphQuery` hook defined elsewhere
@@ -57,30 +64,41 @@ const DatePickerRange: React.FC<{
 
   // setGraphData(data);
   // setUnit(getUnit(diffDays));
-  if(custom){
-    datesQuery(startDate, endDate)
+  if (custom) {
+    datesQuery(startDate, endDate);
   }
 
   const onChange = (dates: [Date, Date]) => {
     const [start, end] = dates;
+    start.setHours(0, 0, 0, 0);
+    end.setHours(23, 59, 59, 999);
     setStartDate(start);
     setEndDate(end);
-    datesQuery(start, end)
+    datesQuery(start, end);
   };
 
-  const ExampleCustomInput = forwardRef<HTMLInputElement, { value?: string; onClick?: () => void }>(
-    ({ value, onClick }, ref) => (
-      <Button variant='contained' className="example-custom-input" onClick={onClick} ref={ref as React.Ref<HTMLButtonElement>}>
-        {value}
-      </Button>
-    )
-  );
+  const ExampleCustomInput = forwardRef<
+    HTMLInputElement,
+    { value?: string; onClick?: () => void }
+  >(({ value, onClick }, ref) => (
+    <Button
+      variant="contained"
+      className="example-custom-input"
+      onClick={onClick}
+      ref={ref as React.Ref<HTMLButtonElement>}
+    >
+      {value}
+    </Button>
+  ));
 
-  const MyContainer: React.FC<{ className: string; children: React.ReactNode }> = ({ className, children }) => {
+  const MyContainer: React.FC<{
+    className: string;
+    children: React.ReactNode;
+  }> = ({ className, children }) => {
     return (
       <div>
         <CalendarContainer className={className}>
-          <div style={{ display: 'flex' }}>
+          <div style={{ display: "flex" }}>
             <div style={{ position: "relative" }}>{children}</div>
           </div>
         </CalendarContainer>
@@ -100,6 +118,6 @@ const DatePickerRange: React.FC<{
       customInput={<ExampleCustomInput />}
     />
   );
-}
+};
 
 export default DatePickerRange;
